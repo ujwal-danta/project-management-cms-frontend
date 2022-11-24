c46dd2476105fb45133ce1c7b69a89aa341ce5fa
ujwal-danta <ujwaldanta@gmail.com>
2022-11-24 21:43:57 +0530

    card container

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import styles from '../styles/Card.module.css'
+import Image from 'next/image'
+import image from '../designs/Screenshot (99).png'
+import Tag from './Tag'
 
 const Card = () => {
     return (
-        <div className={styles.container}>Card</div>
+        <div className={styles.container}>
+            <div className={styles.image_container}>
+                <Image
+                    src={image}
+                    alt='Not Found'
+                    className="image"
+                />
+            </div>
+            <div className={styles.title_container}>
+                <p>Card Title</p>
+            </div>
+            <div className={styles.tag_container}>
+                <Tag />
+                <Tag />
+                <Tag />
+            </div>
+        </div>
     )
 }
 
---
+++ components/Tag.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Card.module.css'
+
+const Tag = () => {
+    return (
+        <div className={styles.tags}>
+            <p>tag</p>
+        </div>
+    )
+}
+
+export default Tag
