9f6b886875ca62237634ee1c383964ce9b5a9abc
ujwal-danta <ujwaldanta@gmail.com>
2022-11-26 21:26:58 +0530

    working on dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../styles/Dashboard.module.css'
+import Image from 'next/image'
+import img from '../designs/ujwal.jpg'
+
 
 const dashboard = () => {
     return (
-        <Layout>dashboard</Layout>
+        <Layout>
+            <div className={styles.dashboard_container}>
+                <div className={styles.side_container}>
+                    <div className={styles.image_container}>
+                        <div className={styles.img}>
+                            <Image src={img} width={70} height={70} className={styles.image} />
+                        </div>
+                        <div className={styles.text_container}>
+                            <p className={styles.text}>Hi Ujwal :) </p>
+                        </div>
+                    </div>
+                    <div className={styles.add_items}>
+                        <p>Hi add items</p>
+                    </div>
+                </div>
+                <div className={styles.main_container}>
+
+                </div>
+
+            </div>
+        </Layout>
     )
 }
 
