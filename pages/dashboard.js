8db0a473f9d14d9f79b6e8d28134cb6bf624d701
ujwal-danta <ujwaldanta@gmail.com>
2023-02-03 17:10:29 +0530

    working on dashboard sidebar

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -21,22 +21,38 @@ const dashboard = () => {
                             <p className={styles.text}>Hi Ujwal :) </p>
                         </div>
                     </div>
-                    <div className={styles.add_items}>
-                        <select name="cars" id="cars" className={styles.select}>
-                            <option value="volvo">ADD AN ITEM</option>
-                            <option value="saab">UPDATE</option>
-                            <option value="opel">DELETE</option>
-                        </select>
-                    </div>
-                    <div className={styles.add_items}>
-                        <select name="cars" id="cars" className={styles.select}>
-                            <option value="volvo">ADD CATEGORY</option>
-                            <option value="opel">DELETE</option>
-                        </select>
-                    </div>
+
+                    {/* side nav */}
+                    <ul className={styles.main_menu}>
+                        <li className={styles.projects}>
+                            <Link href={'#'}>
+                                <a>Projects</a>
+                            </Link>
+                            <ul className={styles.sub_menu}>
+                                <li>Add a project</li>
+                                <li>Delete a project</li>
+                                <li>Update a project</li>
+                            </ul>
+                        </li>
+
+                        <li className={styles.categories}>
+                            <Link href={'#'}>
+                                <a>Categories</a>
+                            </Link>
+                            <ul className={styles.sub_menu}>
+                                <li>Add a category</li>
+                                <li>Delete a category</li>
+                            </ul>
+                        </li>
+
+                    </ul>
+
+
                 </div>
                 <div className={styles.main_container}>
                     <form className={styles.form}>
+                        <h1>Add an Item</h1>
+                        <hr />
                         <div className={styles.form_element}>
                             <label>Add Title</label> <br />
                             <input type="text" className={styles.input} />
